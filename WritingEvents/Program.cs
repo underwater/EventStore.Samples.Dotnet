1b00f3911a1deeb281dc8d8b2e6dd2fb2ef2d4e2
Greg Young <gregoryyoung1@gmail.com>
2014-12-29 14:24:47 -0800

    add WritingEvents sample

--- WritingEvents/Program.cs
+++ WritingEvents/Program.cs
@@ -1,15 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using EventStore.ClientAPI;
 
 namespace WritingEvents
 {
+    /*
+     * This example writes 100 events to the event store. The events will be written to the stream STREAM.
+     * 
+     * As written it will use the default ipaddress (loopback) and the default tcp port 1113 of the event
+     * store. In order to run the application bring up the event store in another window (you can use
+     * default arguments eg EventStore.ClusterNode.exe) then you can run this application with it.
+     * 
+     * The events are in the form of
+     * {
+     *     somedata : <incrementinginteger>
+     * }
+     * 
+     * with metadata of:
+     * {
+     *     metadata : <incrmentinginteger>
+     * }
+     */
     class Program
     {
         static void Main(string[] args)
         {
+            const string STREAM = "writing_test";
+            const int DEFAULTPORT = 1113;
+            var settings = ConnectionSettings.Create().EnableVerboseLogging().UseConsoleLogger();
+            using (var conn = EventStoreConnection.Create(settings, new IPEndPoint(IPAddress.Loopback, DEFAULTPORT)))
+            {
+                conn.ConnectAsync().Wait();
+                for (var x = 0; x < 100; x++)
+                {
+                    conn.AppendToStreamAsync(STREAM,
+                        ExpectedVersion.Any,
+                        GetEventDataFor(x)).Wait();
+                    Console.WriteLine("event " + x + " written.");
+                }
+            }
+        }
+
+        private static EventData GetEventDataFor(int i)
+        {
+            return new EventData(
+                Guid.NewGuid(),
+                "eventType",
+                true,
+                Encoding.ASCII.GetBytes("{'somedata' : " + i + "}"),
+                Encoding.ASCII.GetBytes("{'metadata' : " + i + "}")
+                );
         }
     }
 }
